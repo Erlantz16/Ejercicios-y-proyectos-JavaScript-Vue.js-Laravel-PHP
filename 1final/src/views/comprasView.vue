e8af0f39069f58a972e13833fc2170173e21fc0e
Erlantz <erlantzama@gmail.com>
2024-01-26 14:21:14 +0100

    algo

--- 1final/src/views/comprasView.vue
+++ 1final/src/views/comprasView.vue
@@ -1,94 +1,91 @@
-    <template>
-        <h1>has entrado como {{this.usuarioentrante}}</h1>
-        <div class="productos">
-            <table>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Descripcion</th>
-                    <th>Categoria</th>
-                    <th>Stock</th>
-                    <th>Precio</th>
-                </tr>
-                <tr v-for="(products, index) in productos" :key="index">
-                    <td>{{products.title}}</td>
-                    <td>{{products.description}}</td>
-                    <td>{{products.category}}</td>
-                    <td>{{products.stock}}</td>
-                    <td>{{products.price}}</td>
-                    <td><button @click="añadir(index)">añadir</button></td>
-                </tr>
-            </table>
-        </div>
-        <div>
-            <h1>Carrito</h1>
-            <table>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Descripcion</th>
-                    <th>Categoria</th>
-                    <th>Stock</th>
-                    <th>Precio</th>
-                </tr>
-                <tr v-for="(producto, index) in carrito" :key="index">
-                    <td>{{producto.title}}</td>
-                    <td>{{producto.description}}</td>
-                    <td>{{producto.category}}</td>
-                    <td>{{producto.stock}}</td>
-                    <td>{{producto.price}}</td>
-                </tr>
-            </table>
-        </div>
-
-
-    </template>
-    <script>
-    import axios from 'axios';
-    export default {
-        name: 'comprasView',
-        data() {
-            return {
-                productos: [],
-                carrito: {
-
-                    titulo: '',
-                    descripcion: '',
-                    categoria: '',
-                    stock: '',
-                    precio: '',
-
-                },
-                total: 0,
-                usuario: '',
-                usuarioentrante: '',
-                
-            }
-        },
-
-        mounted() {
-
-
-            this.usuarioentrante = sessionStorage.getItem('usuario');
-
-            axios.get('/public/json/products.json')
-                .then(response => {
-                    this.productos = response.data.products;
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        },
-        methods: {
-            añadir(index) {
-                this.carrito.titulo = this.productos[index].title;
-                this.carrito.descripcion = this.productos[index].description;
-                this.carrito.categoria = this.productos[index].category;
-                this.carrito.stock = this.productos[index].stock;
-                this.carrito.precio = this.productos[index].price;
-                
-
-        }
-        }
-    }
-
-
-    </script>
+<template>
+    <div>
+      <h1>Has entrado como {{ usuarioentrante }}</h1>
+      <div class="productos">
+        <h2>Productos</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Descripción</th>
+              <th>Categoría</th>
+              <th>Stock</th>
+              <th>Precio</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(producto, index) in productos" :key="index">
+              <td>{{ producto.title }}</td>
+              <td>{{ producto.description }}</td>
+              <td>{{ producto.category }}</td>
+              <td>{{ producto.stock }}</td>
+              <td>{{ producto.price }}</td>
+              <td><button @click="añadirAlCarrito(index)">Añadir</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <h2>Carrito</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Descripción</th>
+              <th>Categoría</th>
+              <th>Stock</th>
+              <th>Precio</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(producto, index) in carrito" :key="index">
+              <td>{{ producto.title }}</td>
+              <td>{{ producto.description }}</td>
+              <td>{{ producto.category }}</td>
+              <td>{{ producto.stock }}</td>
+              <td>{{ producto.price }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'comprasView',
+    data() {
+      return {
+        productos: [],
+        carrito: [],
+        usuarioentrante: '',
+      };
+    },
+    mounted() {
+      this.usuarioentrante = sessionStorage.getItem('usuario');
+      axios
+        .get('/public/json/products.json')
+        .then((response) => {
+          this.productos = response.data.products;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    methods: {
+      añadirAlCarrito(index) {
+        const productoSeleccionado = this.productos[index];
+        this.carrito.push({
+          title: productoSeleccionado.title,
+          description: productoSeleccionado.description,
+          category: productoSeleccionado.category,
+          stock: productoSeleccionado.stock,
+          price: productoSeleccionado.price,
+        });
+      },
+    },
+  };
+  </script>
