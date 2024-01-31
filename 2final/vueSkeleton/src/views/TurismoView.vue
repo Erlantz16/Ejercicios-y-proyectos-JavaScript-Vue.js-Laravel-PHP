35fa52fb8481bdef2093f512369556083a349aac
Erlantz16 <102513331+Erlantz16@users.noreply.github.com>
2024-01-31 19:22:05 +0100

    comienzo examen 2final --TourismoView solo

---
+++ 2final/vueSkeleton/src/views/TurismoView.vue
@@ -0,0 +1,132 @@
+<template>
+    <div>
+
+        <select v-model="selectedPlace">
+            <option value="">Cualquiera</option>
+            <option v-for="(place, index) in sitios" :key="index" :value="place">
+            {{ place }}
+        </option>
+        </select>
+    <!-- Las opciones select mes para realizar el filtrado son: Elige una opción (por defecto), Enero, febrero, marzo….. Si seleccionamos “Todos” se mostrarán los eventos de todos los meses, al seleccionar “Enero” se mostrarán los de enero, “febrero” → febrero…. -->
+    <select v-model="selectedMonth">
+        <option value="">Cualquiera</option>
+        <option v-for="(month, index) in months" :key="index" :value="month">
+            {{ month }}
+        </option>
+    </select>
+   
+ <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(dato, index) in filteredMonth" :key="dato.name" :class="{ 'bizkaia': dato.territory === 'Bizkaia', 'gipuzkoa': dato.territory === 'Gipuzkoa', 'araba': dato.territory === 'Araba', 'araba-bizkaia-gipuzkoa': dato.territory ==='Araba Bizkaia Gipuzkoa' ,'bizkaia-araba-gipuzkoa': dato.territory ==='Bizkaia Araba Gipuzkoa'}">
+          <td>{{ dato.documentName }}</td>
+          <td>{{ dato.eventStartDate }}</td>
+
+          <td>{{ dato.territory }}</td>
+          <td>{{ dato.documentDescription }}</td>
+          <!-- <td>{{ dato.ability }}</td> -->
+          <td>{{dato.friendlyUrl}}</td>
+        </tr>
+      </tbody>
+    </table>
+</div>
+
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'TurismoView',
+  data () {
+    return {
+        turismo: [],
+        selectedMonth: '',
+        selectedPlace: '',
+        months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+    sitios() {
+      return [...new Set(this.turismo.map((dato) => dato.territory))];
+
+    },
+    
+    filteredMonth() {
+        return this.turismo
+            .filter(dato => {
+                if (this.selectedMonth === '') {
+                    return true;
+                } else {
+                    let date = new Date(dato.eventStartDate.split('/').reverse().join('-'));
+                    let monthIndex = this.months.indexOf(this.selectedMonth);
+                    return date.getMonth() === monthIndex;
+                }
+            })
+            .filter(dato => {
+                if (this.selectedPlace === '') {
+                    return this.turismo;
+                } else {
+                    return dato.territory === this.selectedPlace;
+                }
+            });
+
+    },
+    filteredPlace() {
+        if (this.selectedPlace === '') {
+        return this.turismo;
+    } else {
+        console.log(this.selectedPlace);
+
+        return this.turismo.filter(
+            dato => { return dato.territory === this.selectedPlace;
+        });
+
+    }
+    },
+    
+  },
+  created () {
+    axios
+        .get('/json/agenda.json')
+        .then(response => {
+            this.turismo = response.data
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    
+  }
+  
+}
+</script>
+
+<style>
+
+.bizkaia {
+    background-color: green;
+}
+
+.gipuzkoa {
+    background-color: brown;
+}
+
+.araba {
+    background-color: red;
+}
+.araba-bizkaia-gipuzkoa {
+    background-color: grey;
+}
+.bizkaia-araba-gipuzkoa {
+    background-color: purple;
+}
+</style>
